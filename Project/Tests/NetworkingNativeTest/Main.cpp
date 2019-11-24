b1340b4f9a1110e25110b40498d41c97265c5b2b
Omid Shahbazi <sh.omid.m@gmail.com>
2019-11-24 10:28:25 +0330

    Modified   Project/NetworkingNative/Compile/SocketUtilities.cpp Modified   Project/Tests/NetworkingNativeTest/Main.cpp Modified   Project/Binaries/NetworkingNative.dll

--- Project/Tests/NetworkingNativeTest/Main.cpp
+++ Project/Tests/NetworkingNativeTest/Main.cpp
@@ -8,5 +8,7 @@ using namespace GameFramework::Networking;
 void main()
 {
 	ServerSocket socket(PlatformNetwork::IPProtocols::TCP, 32);
-	socket.Bind("127.0.0.1", 80);
+	socket.Bind("localhost", 80);
+
+	PlatformNetwork::Shutdown();
 }
