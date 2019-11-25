9013e8f1346ecbfe2e46e5951cb95365a2e76ea4
Omid Shahbazi <sh.omid.m@gmail.com>
2019-11-26 00:04:42 +0330

    Modified   Project/Binaries/NetworkingNative.dll Modified   Project/Binaries/NetworkingNative.lib Modified   Project/NetworkingNative/Compile/SocketUtilities.cpp Modified   Project/Tests/NetworkingNativeTest/Main.cpp

--- Project/NetworkingNative/Compile/SocketUtilities.cpp
+++ Project/NetworkingNative/Compile/SocketUtilities.cpp
@@ -84,102 +84,106 @@ namespace GameFramework::Networking
 
 	IPAddress SocketUtilities::ResolveDomain(const string& Domain)
 	{
-		ADDRINFO info;
-		ZeroMemory(&info, sizeof(info));
-		info.ai_flags = 0;
-		info.ai_family = AF_UNSPEC;
+		ADDRINFO hintInfo;
+		ZeroMemory(&hintInfo, sizeof(ADDRINFO));
+		hintInfo.ai_flags = 0;
+		hintInfo.ai_family = AF_UNSPEC;
 
-		PADDRINFOA infos;
+		PADDRINFOA queryResult;
+		int32_t host = getaddrinfo(Domain.c_str(), "", &hintInfo, &queryResult);
 
-		int32_t host = getaddrinfo(Domain.c_str(), "", &info, &infos);
+		PADDRINFOA result = queryResult;
 
-
-
-		for (addrinfo* ptr = infos; ptr != NULL; ptr = ptr->ai_next)
+		for (addrinfo* ptr = queryResult; ptr != NULL; ptr = ptr->ai_next)
 		{
-			printf("\tFlags: 0x%x\n", ptr->ai_flags);
-			printf("\tFamily: ");
-			switch (ptr->ai_family) {
-			case AF_UNSPEC:
-				printf("Unspecified\n");
-				break;
-			case AF_INET:
-				printf("AF_INET (IPv4)\n");
-				sockaddr_in* sockaddr_ipv4 = (struct sockaddr_in*) ptr->ai_addr;
-				//printf("\tIPv4 address %s\n", inet_ntop(ptr->ai_family, sockaddr_ipv4->sin_addr));
-				break;
-			case AF_INET6:
-				printf("AF_INET6 (IPv6)\n");
-				// the InetNtop function is available on Windows Vista and later
-				// sockaddr_ipv6 = (struct sockaddr_in6 *) ptr->ai_addr;
-				// printf("\tIPv6 address %s\n",
-				//    InetNtop(AF_INET6, &sockaddr_ipv6->sin6_addr, ipstringbuffer, 46) );
+			if (ptr->ai_family != AF_INET6)
+				continue;
 
-				// We use WSAAddressToString since it is supported on Windows XP and later
-				LPSOCKADDR sockaddr_ip = (LPSOCKADDR)ptr->ai_addr;
-				// The buffer length is changed by each call to WSAAddresstoString
-				// So we need to set it for each iteration through the loop for safety
-				DWORD ipbufferlength = 46;
-				wchar_t ipstringbuffer[46];
+			result = ptr;
+			break;
+			//printf("\tFlags: 0x%x\n", ptr->ai_flags);
+			//printf("\tFamily: ");
+			//switch (ptr->ai_family) {
+			//case AF_UNSPEC:
+			//	printf("Unspecified\n");
+			//	break;
+			//case AF_INET:
+			//	printf("AF_INET (IPv4)\n");
+			//	sockaddr_in* sockaddr_ipv4 = (struct sockaddr_in*) ptr->ai_addr;
+			//	//printf("\tIPv4 address %s\n", inet_ntop(ptr->ai_family, sockaddr_ipv4->sin_addr));
+			//	break;
+			//case AF_INET6:
+			//	printf("AF_INET6 (IPv6)\n");
+			//	// the InetNtop function is available on Windows Vista and later
+			//	// sockaddr_ipv6 = (struct sockaddr_in6 *) ptr->ai_addr;
+			//	// printf("\tIPv6 address %s\n",
+			//	//    InetNtop(AF_INET6, &sockaddr_ipv6->sin6_addr, ipstringbuffer, 46) );
 
-				INT iRetval = WSAAddressToStringW(sockaddr_ip, (DWORD)ptr->ai_addrlen, NULL,
-					ipstringbuffer, &ipbufferlength);
-				if (iRetval)
-					printf("WSAAddressToString failed with %u\n", WSAGetLastError());
-				else
-					printf("\tIPv6 address %s\n", ipstringbuffer);
-				break;
-			case AF_NETBIOS:
-				printf("AF_NETBIOS (NetBIOS)\n");
-				break;
-			default:
-				printf("Other %ld\n", ptr->ai_family);
-				break;
-			}
-			printf("\tSocket type: ");
-			switch (ptr->ai_socktype) {
-			case 0:
-				printf("Unspecified\n");
-				break;
-			case SOCK_STREAM:
-				printf("SOCK_STREAM (stream)\n");
-				break;
-			case SOCK_DGRAM:
-				printf("SOCK_DGRAM (datagram) \n");
-				break;
-			case SOCK_RAW:
-				printf("SOCK_RAW (raw) \n");
-				break;
-			case SOCK_RDM:
-				printf("SOCK_RDM (reliable message datagram)\n");
-				break;
-			case SOCK_SEQPACKET:
-				printf("SOCK_SEQPACKET (pseudo-stream packet)\n");
-				break;
-			default:
-				printf("Other %ld\n", ptr->ai_socktype);
-				break;
-			}
-			printf("\tProtocol: ");
-			switch (ptr->ai_protocol) {
-			case 0:
-				printf("Unspecified\n");
-				break;
-			case IPPROTO_TCP:
-				printf("IPPROTO_TCP (TCP)\n");
-				break;
-			case IPPROTO_UDP:
-				printf("IPPROTO_UDP (UDP) \n");
-				break;
-			default:
-				printf("Other %ld\n", ptr->ai_protocol);
-				break;
-			}
-			printf("\tLength of this sockaddr: %d\n", ptr->ai_addrlen);
-			printf("\tCanonical name: %s\n", ptr->ai_canonname);
+			//	// We use WSAAddressToString since it is supported on Windows XP and later
+			//	LPSOCKADDR sockaddr_ip = (LPSOCKADDR)ptr->ai_addr;
+			//	// The buffer length is changed by each call to WSAAddresstoString
+			//	// So we need to set it for each iteration through the loop for safety
+			//	DWORD ipbufferlength = 46;
+			//	wchar_t ipstringbuffer[46];
+
+			//	INT iRetval = WSAAddressToStringW(sockaddr_ip, (DWORD)ptr->ai_addrlen, NULL,
+			//		ipstringbuffer, &ipbufferlength);
+			//	if (iRetval)
+			//		printf("WSAAddressToString failed with %u\n", WSAGetLastError());
+			//	else
+			//		printf("\tIPv6 address %s\n", ipstringbuffer);
+			//	break;
+			//case AF_NETBIOS:
+			//	printf("AF_NETBIOS (NetBIOS)\n");
+			//	break;
+			//default:
+			//	printf("Other %ld\n", ptr->ai_family);
+			//	break;
+			//}
+			//printf("\tSocket type: ");
+			//switch (ptr->ai_socktype) {
+			//case 0:
+			//	printf("Unspecified\n");
+			//	break;
+			//case SOCK_STREAM:
+			//	printf("SOCK_STREAM (stream)\n");
+			//	break;
+			//case SOCK_DGRAM:
+			//	printf("SOCK_DGRAM (datagram) \n");
+			//	break;
+			//case SOCK_RAW:
+			//	printf("SOCK_RAW (raw) \n");
+			//	break;
+			//case SOCK_RDM:
+			//	printf("SOCK_RDM (reliable message datagram)\n");
+			//	break;
+			//case SOCK_SEQPACKET:
+			//	printf("SOCK_SEQPACKET (pseudo-stream packet)\n");
+			//	break;
+			//default:
+			//	printf("Other %ld\n", ptr->ai_socktype);
+			//	break;
+			//}
+			//printf("\tProtocol: ");
+			//switch (ptr->ai_protocol) {
+			//case 0:
+			//	printf("Unspecified\n");
+			//	break;
+			//case IPPROTO_TCP:
+			//	printf("IPPROTO_TCP (TCP)\n");
+			//	break;
+			//case IPPROTO_UDP:
+			//	printf("IPPROTO_UDP (UDP) \n");
+			//	break;
+			//default:
+			//	printf("Other %ld\n", ptr->ai_protocol);
+			//	break;
+			//}
+			//printf("\tLength of this sockaddr: %d\n", ptr->ai_addrlen);
+			//printf("\tCanonical name: %s\n", ptr->ai_canonname);
 		}
 
-		return IPAddress(PlatformNetwork::AddressFamilies::InterNetwork, 0);
+		return IPAddress(PlatformNetwork::AddressFamilies::InterNetwork,  0);
 
 		//try
 		//{
--- Project/Tests/NetworkingNativeTest/Main.cpp
+++ Project/Tests/NetworkingNativeTest/Main.cpp
@@ -8,7 +8,7 @@ using namespace GameFramework::Networking;
 void main()
 {
 	ServerSocket socket(PlatformNetwork::IPProtocols::TCP, 32);
-	socket.Bind("::1", 80);
+	socket.Bind("193.176.243.149", 80);
 
 	PlatformNetwork::Shutdown();
 }
