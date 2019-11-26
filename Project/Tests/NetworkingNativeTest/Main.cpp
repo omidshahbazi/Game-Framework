a18e56d29b62ed263956c6f27a64e71bcc4b908b
Omid Shahbazi <sh.omid.m@gmail.com>
2019-11-26 23:29:49 +0330

    Modified   Project/Binaries/NetworkingManaged.NetCore.dll Modified   Project/Binaries/NetworkingNative.dll Modified   Project/NetworkingNative/Compile/SocketUtilities.cpp Modified   Project/Tests/NetworkingNativeTest/Main.cpp

--- Project/Tests/NetworkingNativeTest/Main.cpp
+++ Project/Tests/NetworkingNativeTest/Main.cpp
@@ -8,7 +8,8 @@ using namespace GameFramework::Networking;
 void main()
 {
 	ServerSocket socket(PlatformNetwork::IPProtocols::TCP, 32);
-	socket.Bind("193.176.243.149", 80);
+	//socket.Bind("193.176.243.149", 80);
+	socket.Bind("google.com", 80);
 
 	PlatformNetwork::Shutdown();
 }
